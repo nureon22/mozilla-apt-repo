073f93395372d05898a42a2b244689232f9484a1
nureon22 <147523053+nureon22@users.noreply.github.com>
2024-04-12 02:36:23 +0630

    Improve error handling and readability

--- install.sh
+++ install.sh
@@ -2,19 +2,20 @@
 
 set -e
 
+ERROR_COLOR="\033[31m"
+WARNING_COLOR="\033[33m"
+RESET_COLOR="\033[0m"
+
 REPO_SIGNING_KEY="https://packages.mozilla.org/apt/repo-signing-key.gpg"
 REPO_SIGNING_KEY_LOCATION="/etc/apt/keyrings/packages.mozilla.org.asc"
 
 prompt_user() {
-    while true; do
-        echo "Root permission is required to create new APT repository"
-        read -p "Do you want to continue? (y|N) " answer
-        case $answer in
-            [Yy]*) break ;;
-            [Nn]*) exit ;;
-            *) exit;;
-        esac
-    done
+    echo "Root permission is required to create new APT repository"
+    read -p "Do you want to continue? (y|N) " answer
+    case ${answer:0:1} in
+        [Yy]) return 0 ;;
+        *) return 1 ;;
+    esac
 }
 
 # Check is a given command exists
@@ -26,7 +27,7 @@ command_exists() {
 # First argument is command and second argument is package.
 install_package() {
     if ! command_exists "$1"; then
-        echo "Command '$1' not found, but can be instal with:"
+        echo "Command '$1' not found, but can be install with:"
         echo "apt-get install $2"
         exit 1
     fi
@@ -37,25 +38,25 @@ import_signing_key() {
     # Create a directory to store APT repository keys
     [ ! -d /etc/apt/keyrings ] && sudo install -d -m 0755 /etc/apt/keyrings
 
+    local downloader=""
+
     if command_exists curl; then
-        curl -sL "$REPO_SIGNING_KEY" | sudo tee "$REPO_SIGNING_KEY_LOCATION" > /dev/null
+        downloader="curl -sL"
     elif command_exists wget; then
-        wget -q -O - "$REPO_SIGNING_KEY" | sudo tee "$REPO_SIGNING_KEY_LOCATION" > /dev/null
+        downloader="wget -q -O-"
     else
-        echo "You need to install curl or wget first."
-        echo "Run: apt-get install curl"
+        echo "${ERROR_COLOR}Error: Neither curl nor wget found. Please install one of them first.${RESET_COLOR}"
         exit 1
     fi
+    $downloader "$REPO_SIGNING_KEY" | sudo tee "$REPO_SIGNING_KEY_LOCATION" > /dev/null
 }
 
-# The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3
 verify_fingerprint() {
-    LOCAL_FINGERPRINT=$(gpg -n -q --import --import-options import-show "$REPO_SIGNING_KEY_LOCATION" | awk '/pub/{getline; gsub(/^ +| +$/,""); print $1}')
+    local EXPECTED_FINGERPRINT="35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3"
+    local LOCAL_FINGERPRINT=$(gpg -n -q --import --import-options import-show "$REPO_SIGNING_KEY_LOCATION" | awk '/pub/{getline; gsub(/^ +| +$/,""); print $1}')
 
-    if [ $LOCAL_FINGERPRINT == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3" ]; then
-        echo -e "\nThe key finterprint match (\"$LOCAL_FINGERPRINT\").\n"
-    else
-        echo -e "\nVerification failed: the fingerprint (\"$LOCAL_FINGERPRINT\") does not match the expected one.\n"
+    if [ "$LOCAL_FINGERPRINT" != "$EXPECTED_FINGERPRINT" ]; then
+        echo -e "${ERROR_COLOR}Error: Verification failed: the fingerprint mismatch.${RESET_COLOR}"
         exit 1
     fi
 }
@@ -68,7 +69,10 @@ add_and_configure_apt_repo() {
     echo -e "Package: *\nPin: origin packages.mozilla.org\nPin-Priority: 900" | sudo tee /etc/apt/preferences.d/mozilla > /dev/null
 }
 
-prompt_user
+if ! prompt_user; then
+    exit 0;
+fi
+
 install_package sudo sudo
 install_package gpg gnupg
 sudo true
